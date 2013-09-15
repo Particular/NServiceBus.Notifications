462c9a92f535ed9edd915e0123ba3fe3d42d56f3
Simon Cropp <simon.cropp@gmail.com>
2013-09-16 09:15:36 +1000

    move sample over

---
+++ src/Sample/EmailSender.cs
@@ -0,0 +1,25 @@
+namespace NotifySample
+{
+    using System;
+    using NServiceBus;
+
+    public class EmailSender : IWantToRunWhenBusStartsAndStops
+    {
+        public IBus Bus { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Hit any key to send a email using the notification satellite");
+
+            while (Console.ReadKey().Key.ToString().ToLower() != "q")
+            {
+                Bus.SendEmail(new MailMessage("X", "Y", "Hello from the the NSB notification support", "The body"));
+            }
+        }
+
+        public void Stop()
+        {
+            //no-op
+        }
+    }
+}
